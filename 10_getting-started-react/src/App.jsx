b299cae3ed93539a2625750df27d1043feb3c838
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-08-26 10:28:07 +1200

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+<main style={{ fontFamily: "Fira Code, monospace", padding: "2rem", lineHeight: 1.6 }}>      <h1>About Me</h1>
+      <p><strong>Name:</strong> Veneese Julia M. Lazaraga </p>
+      <p><strong>Course & Year:</strong> BS Information Systems, 3rd Year</p>
+      <p><strong>Fun fact:</strong> I can see things better  at night.</p>
+      <p><strong>Why I want to learn React:</strong> <br/>
+      I want to learn React because it offers tools for building efficient UIs. Since it’s widely used in the industry, mastering it will strengthen my frontend skills and allow me to apply them to real-world projects.</p>
+    </main>
+  );
+}
+export default App;
