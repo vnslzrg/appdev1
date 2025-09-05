4beb6fba1b2b2b06a51dd5aeb21a0eaf274eccb7
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-09-05 22:40:04 +1200

    Add base syntax activity

---
+++ 11_javascript-refresher/01_base_syntax.js
@@ -0,0 +1,13 @@
+console.log("Hello Javascript");
+
+
+let myName = "Veneese";
+let myname = "Julia";
+
+console.log(myName);
+console.log(myname);
+
+//kahit same sila na myname nakikita sila ni javascript as separate identifiers
+//case sensitive ang javascript
+//camel case ang ginagamit sa pag name ng variable sa javascript
+//pwede mag start sa _ or $ pero hindi sa number
