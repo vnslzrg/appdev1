a0858457b457ff164db19eafa6001206c6a408d0
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-09-05 23:13:55 +1200

    Add modules import activity

---
+++ 11_javascript-refresher/16_modules_import.js
@@ -0,0 +1,6 @@
+import greet from './15_modules_export.js';
+import { studentInfo, getStudentInfo } from './15_modules_export.js';       
+
+console.log(greet());
+console.log(studentInfo);
+console.log(getStudentInfo());
