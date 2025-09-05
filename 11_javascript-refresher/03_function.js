1580fd9dbe7547527ccbcae54f33ebb4355ece00
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-09-05 22:51:58 +1200

    Add functions activity

---
+++ 11_javascript-refresher/03_function.js
@@ -0,0 +1,17 @@
+//FUNCTIONS
+function greet(name) {
+    return `Hello, ${name}!`
+}
+console.log(greet("vj"))
+
+//ARROW FUNCTION
+const square = num => num * num
+console.log(square(7))
+
+//CALCULATOR FUNCTION
+function calculator (a , b) {
+    return `sum: ${a+b}
+product: ${a*b}`
+}
+console.log(calculator(5 , 2))
+
