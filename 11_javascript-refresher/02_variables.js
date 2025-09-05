977268ae564d3817e63f53a78ffe168789e73d17
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-09-05 22:42:43 +1200

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,25 @@
+//VARIABLES
+const name = "Veneese";
+const age = 20;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+//TYPEOF
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+
+// ARITHMETIC OPERATORS
+const a = 10, b = 7;
+
+console.log("add:", a + b);
+console.log("subtract:", a - b);
+console.log("multiplication:", a * b);
+console.log("division:", a/b);
+
+//DIFFERENCE BETWEEN == AND ===
+console.log("5" == 5)
+console.log("5" === 5)
