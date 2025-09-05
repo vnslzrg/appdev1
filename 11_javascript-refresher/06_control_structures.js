85ce8d48a1ffd1fc4f8d9e9981ba57a9927da835
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-09-05 22:54:51 +1200

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,27 @@
+//IF, ELSE IF, ELS
+let score = 99; 
+
+if (score >= 90) {   
+    console.log("Grade: A")
+}
+else if (score >= 80) {
+    console.log("Grade: B")
+} 
+else if (score >= 70) {
+    console.log("Grade: C")
+}
+else {
+    console.log("Grade: F")
+}   
+
+//FOR LOOP
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+//WHILE LOOP
+let count = 0;      
+while (count < 3) {
+    console.log("Hello");
+    count++;
+}
+
