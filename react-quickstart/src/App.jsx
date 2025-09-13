9a0518827d93fa82f2dc6a86cd08faf5818b462e
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-09-13 10:39:28 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -2,10 +2,7 @@ import './App.css'
 
 function AboutPage() {
   return (
-    <>
-    <h1>About</h1>
-    <p>Hello there.<br /> How do you do?</p>
-    </>
+    <img className="avatar" src ="https://i.imgur.com/yXOvdOSs.jpg"/>
   );
 }
 
