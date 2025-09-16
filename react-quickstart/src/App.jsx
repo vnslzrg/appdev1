8aa975280e4a8f72472c151494ef783ea815f707
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-09-16 23:17:39 +0800

    Done conditional rendering

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,25 +1,30 @@
 import './App.css'
 
-const user = {
-  name: 'Veneese',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90, 
+function AdminPanel() {
+  return <h1>Welcome, Admin!</h1>;
+}
+
+function LoginForm() {
+  return <h1>Please log in to continue.</h1>;
 }
 
 function App() {
-return (
-  <>
-    <h1>{user.name}</h1>
-    <img
-      className="avatar"
-      src={user.imageUrl}
-      alt={'Photo of ' + user.name}
-      style={{ 
-        width: user.imageSize,
-        height: user.imageSize 
-    }}
-    />
-  </>
-)
+const isLoggedIn = true; 
+
+let content;
+
+if (isLoggedIn){
+  content = <AdminPanel />;
 }
+else {
+  content = <LoginForm />;
+}
+
+return (
+  <div>
+    {content}
+  </div>
+);
+}
+
 export default App
