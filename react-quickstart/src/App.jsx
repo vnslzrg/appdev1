90b2cd4813359d095ef4d2fc85abc79444590f13
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-09-19 21:25:48 +0800

    Done sharing data between components

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,30 +1,32 @@
 import './App.css'
 import { useState } from 'react';
 
-
-function MyButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
+function MyButton({ count, onClick }) {
   return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
+	<button onClick={onClick}>
+  	Clicked {count} times
+	</button>
   );
 }
 
-  export default function MyApp() {
-  return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
+export default function MyApp() {
+ const [count, setCount] = useState(0);
+
+ function handleClick() {
+   setCount(count + 1);
+ }
+ return (
+	<div>
+  	<h1>Counters that update together</h1>
+  	<MyButton count={count} onClick={handleClick} />
+  	<MyButton count={count} onClick={handleClick} />
+	</div>
   );
 }
+
+
+
+
 
 
 
