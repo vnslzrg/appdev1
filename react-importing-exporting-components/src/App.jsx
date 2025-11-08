19cf06d8eea8823d1d20f9c6bc2e69e924fcf333
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-11-09 01:26:31 +0800

    Done with the root component file

---
+++ react-importing-exporting-components/src/App.jsx
@@ -0,0 +1,21 @@
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h2>Amazing scientists</h2>
+      <div className="image-row">
+      <Profile />
+      <Profile />
+      <Profile />
+      </div>
+    </section>
+  );
+}
