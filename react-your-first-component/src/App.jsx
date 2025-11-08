a75e49b6b6ea945821f10503bb1d3b30dbed1d61
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-11-09 01:06:00 +0800

    Done with using a component

--- react-your-first-component/src/App.jsx
+++ react-your-first-component/src/App.jsx
@@ -1,8 +1,23 @@
-export default function Profile() {
+import './App.css';
+
+function Profile() {
   return (
     <img
-      src="https://i.imgur.com/MK3eW3Am.jpg"
+      src="https://i.imgur.com/MK3eW3As.jpg"
       alt="Katherine Johnson"
     />
-  )
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h2>Amazing scientists</h2>
+      <div className="image-row">
+      <Profile />
+      <Profile />
+      <Profile />
+      </div>
+    </section>
+  );
 }
