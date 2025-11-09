4c5363a45ced64452d6ea8702c6731a14e7c6da3
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-11-09 16:01:46 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/App.jsx
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -1,17 +1,21 @@
 export default function TodoList() {
   return (
-    <>
+    <div> 
       <h1>Hedy Lamarr's Todos</h1>
       <img
         src="https://i.imgur.com/yXOvdOSs.jpg" 
         alt="Hedy Lamarr" 
-        class="photo"
-      />
+        className="photo"
+      /> 
       <ul>
-        <li>Invent new traffic lights</li>
+        <li>Invent new traffic lights</li> 
         <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
+        <li>Improve the spectrum technology</li> 
       </ul>
-    </>
+    </div>
   );
 }
+
+// Added self-closing tag for img and li elements.
+// Uses camelCase for className attribute as per JSX conventions.
+// Added div to wrap content
