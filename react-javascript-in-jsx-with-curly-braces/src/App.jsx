e3879b9e71993c316a93865235de042297eee513
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-11-22 10:17:55 +0800

    Done with using double curlies: CSS and other objects in JSX

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,14 +1,12 @@
-const today = new Date();
-
-function formatDate(date) {
-  return new Intl.DateTimeFormat(
-    'en-US',
-    { weekday: 'long' }
-  ).format(date);
-}
-
 export default function TodoList() {
   return (
-    <h1>To Do List for {formatDate(today)}</h1>
+    <ul style={{
+      backgroundColor: 'black',
+      color: 'pink'
+    }}>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
   );
 }
