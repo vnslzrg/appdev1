49c77fd0521072f5ade49fdff227f38bb37fc337
vns_lzrg <veneesejulialazaraga@gmail.com>
2025-11-22 10:09:46 +0800

    Done with passing strings with quotes

---
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -0,0 +1,25 @@
+//single quote
+// export default function Avatar() {
+//   const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+//   const description = 'Gregorio Y. Zara';
+//   return (
+//     <img
+//       className="avatar"
+//       src={avatar}
+//       alt={description}
+//     />
+//   );
+// }
+
+
+//double quote
+export default function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://i.imgur.com/7vQD0fPs.jpg"
+      alt="Gregorio Y. Zara"
+    />
+  );
+}
+
